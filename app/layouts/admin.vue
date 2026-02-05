d5e8920023a2bd428d4fb1f2edbb64aed1623675
YeiyoNathnael <138720671+YeiyoNathnael@users.noreply.github.com>
2026-02-05 07:11:05 +0400

    feat(admin): add admin dashboard page with stats display
    - Introduced a new admin dashboard page at app/pages/admin/index.vue
    - Integrated useAdminData composable to fetch admin statistics
    - Added DashboardStatCard component to display statistics
    - Updated package.json to include iconify dependency

--- app/layouts/admin.vue
+++ app/layouts/admin.vue
@@ -1,18 +1,51 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = ref<NavigationMenuItem[][]>([
+  [
+    {
+      label: 'Profile',
+      icon: 'tabler:user-circle',
+    },
+    {
+      label: 'Dashboard',
+      icon: 'tabler:browser',
+    },
+    {
+      label: 'Manage Documents',
+      icon: 'tabler:folder-open-filled',
+    },
+    {
+      label: 'Manage Users',
+      icon: 'tabler:users-group',
+    }
+  ],
+ 
+])
+</script>
+
 <template>
+  <div>
+    <UDashboardGroup>
+      <UDashboardSidebar class="bg-amber-50">
+        <template #header>
+          <div class="text-black text-2xl align-middle">
+            Admin Dashboard
+          </div>
+        </template>
+	<template #default>
+					<UNavigationMenu orientation="vertical" :items="items" class="data-[orientation=vertical]:w-48"/>        
+	</template>
 
-  <UContainer>
-  <UDashboardGroup>
-    <UDashboardSidebar class="bg-amber-50">
-	
-				<template #header>
-					<div class="text-black text-3xl align-middle">
-					Admin Dashboard
-					</div>
-				</template>
-    </UDashboardSidebar>
+	<template #footer>
+	<UButton class="w:auto"> Log Out </UButton>
+	</template>
+      </UDashboardSidebar>
 
-    <slot />
-  </UDashboardGroup>
-  </UContainer>
+      <UDashboardPanel>
+        <slot />
+      </UDashboardPanel>
+    </UDashboardGroup>
+  </div>
 </template>
 
